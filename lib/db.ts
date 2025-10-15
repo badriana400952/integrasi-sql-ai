b902cbd26100e107723bd083721e194f4e13cac6
badriana400952 <badriana0402@gmail.com>
2025-10-15 16:42:25 +0700

    db

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,20 @@
 import { Pool } from "pg";
 
-export const Pools = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "warungMakan",
-  password: "123456789",
-  port: 5432,
-});
+const isProd = process.env.NODE_ENV === "production";
+
+export const Pools = new Pool(
+  isProd
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        user: "postgres",
+        host: "localhost",
+        database: "warungMakan",
+        password: "123456789",
+        port: 5432,
+      }
+);
 
 export default Pools;
